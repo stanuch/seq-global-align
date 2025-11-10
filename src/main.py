4369bdd3b7723c806d9d3a9134eab7233b9cb538
Aleksander Stanuch <aleksanderstanuch@gmail.com>
2025-11-10 22:24:03 +0100

    Change files directory and change "sequences" folder path in main.py

--- src/main.py
+++ src/main.py
@@ -4,7 +4,7 @@ from needle import needleman_wunsch
 from Bio import SeqIO
 
 def get_file_path(base_dir, filename):
-    seq_dir = os.path.join(base_dir, ".", "sequences") # fasta files path
+    seq_dir = os.path.join(base_dir, "..", "sequences") # fasta files path
     return os.path.join(seq_dir, f"{filename}.fasta")
 
 def print_sequences(file_path, label):
